185490c676e31af9959eb3d22d1e64976b12a68e
nakkajagadeesh12 <nakkajagadeesh1234@gmail.com>
2019-06-26 16:31:07 +0530

    4 files modified, 3 deleted

--- MainComponent.js
+++ MainComponent.js
@@ -6,9 +6,6 @@ import LoadingIndicator from './LoadingIndicator';
 import './style.css';
 import Table from './Table';
 import NewComponent from './NewComponent';
-import Test from './PureComp';
-import Fun from './funComp';
-import checkBox from './CheckBox';
 
 let flag;
 class MainComponent extends Component {
@@ -38,7 +35,6 @@ class MainComponent extends Component {
       <div className="main-component">
         <button type="button" onClick={this.getData1.bind(this)}>Click to check the Table1</button>
         <button type="button" onClick={this.getData2.bind(this)}>Click to check the Table2</button>
-        <checkBox />
         {
           this.state.click ?
             (this.props.loading ?
--- Table.js
+++ Table.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 import ReactTable from 'react-table';
 import 'react-table/react-table.css';
+import './style.scss';
 
 //React Table
 class Table extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      toggle1: true,
+    }
+  }
+  toggleChange1() {
+    console.log(this.state.toggle1, "tog1")
+    this.setState({
+      toggle1: !this.state.toggle1
+    })
+  }
 
   getData() {
     const data = this.props.tabledata1;
     return data;
   }
+
   getColumns() {
     const columns = [
       {
@@ -46,10 +60,23 @@ class Table extends React.Component {
   render() {
     return (
       <div>
-        <ReactTable
-          data={this.getData()}
-          columns={this.getColumns()}
-        />
+        {console.log(this.state.toggle1, "togggt1")}
+        <div class="ui fitted toggle checkbox" >
+          Toggle1:
+          <input type="checkbox" class="hidden" readonly="" tabindex="0"
+            checked={this.state.toggle1}
+            onClick={this.toggleChange1.bind(this)}
+          />
+          <label></label>
+        </div>
+        <div>
+          <ReactTable
+            data={this.getData()}
+            columns={this.getColumns()}
+            showPaginationBottom={false}
+            defaultPageSize={10}
+          />
+        </div>
       </div>
     )
   }
