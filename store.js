aba297302c7c7f0045277be766b796d507f3cd41
nakkajagadeesh12 <nakkajagadeesh1234@gmail.com>
2019-07-04 12:35:44 +0530

    1 file modified

--- store.js
+++ store.js
@@ -5,6 +5,7 @@ import createSagaMiddleware from 'redux-saga';
 
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(storeReducer, applyMiddleware(sagaMiddleware));
+store.subscribe(() => localStorage.setItem('reduxState', JSON.stringify(store.getState())));
 sagaMiddleware.run(sagaMethod);
 
 export default store;
