b19e71895f91208c1dc17cd0b0ae62cb29606873
nakkajagadeesh12 <44523085+nakkajagadeesh12@users.noreply.github.com>
2020-02-13 13:24:54 +0530

    update reducer

--- reducer.js
+++ reducer.js
@@ -1,36 +1,37 @@
-import { GET_TABLE_DATA, GOT_TABLE_DATA, ERR_TABLE } from './constants';
+import { GET_TABLE_DATA_DATA, GOT_TABLE_DATA, ERR_TABLE } from "./constants";
 
 const initialState = {
-  data: '',
+  data: "",
   loading: false,
-  error: false,
-}
+  error: false
+};
 
 const storeReducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'GET_TABLE_DATA':
+    case "GET_TABLE_DATA_DATA":
       return {
         ...state,
-        loading: true,
-      }
-    case 'GET_SECOND_TABLE':
+        loading: true
+      };
+    case "GET_SECOND_TABLE_DATA":
       return {
         ...state,
-        loading: true,
-      }
-    case 'GOT_TABLE_DATA':
+        loading: true
+      };
+    case "GOT_TABLE_DATA":
       return {
         ...state,
-        data: action.response,
-      }
-    case 'ERR_TABLE':
+        loading: false,
+        data: action.response
+      };
+    case "ERR_TABLE":
       return {
         ...state,
         error: true
-      }
+      };
     default:
       return state;
   }
-}
+};
 
 export default storeReducer;
